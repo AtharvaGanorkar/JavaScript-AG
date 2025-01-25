56e8179ecfc2ea6b2a48f245b207595853bd11aa
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-01-25 02:18:19 +0000

    memory

---
+++ 01_Basics/06_Memories.js
@@ -0,0 +1,10 @@
+// Stack (Primitive) , Heap (Non - Primitive)
+
+let myYoutuename = "Atharva"
+
+let anothername = myYoutuename
+anothername = "Ag"
+
+console.log(myYoutuename);
+console.log(anothername);
+
