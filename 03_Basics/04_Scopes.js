7e33a227799d94c5e762e1dadc647b6d6038c394
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-01-30 06:10:33 +0000

    Scope Leve and mini Hoisting in Js

---
+++ 03_Basics/04_Scopes.js
@@ -0,0 +1,47 @@
+function one(){
+    const username = "Atharva"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+       
+    }
+    // console.log(website);
+
+    two()
+    
+}
+
+one()
+
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " Youtube"
+        console.log(username + website);
+
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+// +++++++++++++++++ intresting +++++++++++++++++++
+
+
+console.log(addone(1))
+
+
+function addone(num){
+        return num + 1
+}
+
+
+addTwo(5)
+
+const addTwo = function(num) {
+    return num + 2
+}
+
