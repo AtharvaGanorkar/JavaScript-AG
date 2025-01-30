e5ac267827aaa01ae6da6294b30edb88e22ffb57
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-01-30 05:43:17 +0000

    Global and Local Scope In JavaScript

---
+++ 03_Basics/03_Scopes.js
@@ -0,0 +1,25 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// { }  --> Scope (when curly brackets comes with conditional 
+                    // Statements or loops )
+
+
+// var c = 300
+   let a = 300    
+
+if (true) {
+    let a = 10
+    const b =20
+    var c = 30
+    console.log("Inner :", a);
+    
+}
+
+console.log(a);
