401d39c6f15846708520c5f4c02f7a98cd3c628c
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-01-31 03:09:13 +0000

    FOR LOOP

---
+++ 05_Iterations/one.js
@@ -0,0 +1,53 @@
+// For Loop
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (element == 5){
+//         console.log("5 is Best number");
+        
+//     }
+//     console.log(element);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//    for (let j = 1; j <= 10; j++) {
+//     //console.log(`Inner loop value ${j} and inner loop ${i}`);
+//     console.log(i + '*' + j + ' = ' + i*j );
+//    }
+// }
+
+// let myArray = ["falsh","Batman","Superman"]
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+// Break and Continue
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5){
+        console.log("Deceted 5");
+        break
+    }
+    console.log(`value of i is ${index}`);
+    
+}
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5){
+        console.log("Deceted 5");
+        continue
+    }
+    console.log(`value of i is ${index}`);
+    
+}
