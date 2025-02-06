b03b75ada75d62ee32927d3a210653ac8ede9ab1
AtharvaGanorkar <atharvaganorkar148@gmail.com>
2025-02-06 01:17:35 +0000

    BMI Calculator

--- DOM_Projects/Project_02/script.js
+++ DOM_Projects/Project_02/script.js
@@ -1,19 +1,32 @@
-const form = document.querySelector('form')
-// This use case will give you empty value 
-// const weigth = parseInt(document.getElementById('weight').Value)
-// const height = parseInt(document.getElementById('height').value)
-// if ypu write it before event , best use it inside event
+const form = document.querySelector('form');
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
 
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
 
-form.addEventListener('submit' , (s) => {
-    s.preventDefault();
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+  const conclusion = document.querySelector('#conclusion');
+  
 
-    const weigth = parseInt(document.querySelector('#weight').Value)
-    const height = parseInt(document.querySelector('#height').value)
-    const Results = document.getElementById('results')
-    
-    if(height === '' || height < 0 || isNaN(height)){
-       Results.innerHTML = "please give a valid height"
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>Your BMI is: ${bmi}</span>`;
+
+    // BMI cateogary
+    if(bmi < 18.6){
+       conclusion.innerHTML = `You are Under Weight`
+    }else if(bmi >= 18.6 && bmi <= 24.9){
+        conclusion.innerHTML = `Your weight is in Normal Range`
+    } else {
+        conclusion.innerHTML = `You are over Weight`
     }
-
-})
+  }
+});
